96393b0e13e58ff9dd0c20334c900fb0edaeb65f
Stephen Thompson <s31@umbc.edu>
2022-02-04 15:12:34 -0600

    basic app with sql database and models

---
+++ sims/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.forms import CharField
+
+class Simulation(models.Model):
+    name = models.CharField(max_length=255)
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    def __str__(self) -> str:
+        return self.name
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    simulation = models.ForeignKey(Simulation, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
